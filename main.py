a9938fc5d087433118627f7da6ff84f256bf1cc9
Rafał Wielądek <rafalwieladek@tlen.pl>
2026-01-28 11:56:06 +0100

    Merge correction.

--- main.py
+++
@@ -1,9 +0,0 @@
-import character_extraction
-
-TEXT_PATH = "./data/pride_and_prejudice_cleaned.txt"
-
-def main():
-    #character_extraction.ner_stanford(TEXT_PATH)
-    character_extraction.ner_spacy(TEXT_PATH)
-if __name__ == "__main__":
-    main()
